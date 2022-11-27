1f669acf1b8b7e1ef1d8ec4d795e521073ead05b
Alex Paul <alexpaul@users.noreply.github.com>
2022-11-27 13:55:31 -0500

    Initial Commit

---
+++ ZwiftRoutes/ZwiftRoutesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZwiftEventsApp.swift
+//  ZwiftEvents
+//
+//  Created by Alex Paul on 11/19/22.
+//
+
+import SwiftUI
+
+@main
+struct ZwiftRoutesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
